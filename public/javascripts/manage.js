0f2e5202faebeaac84637cf19ab0544e9514eca5
Ben Rodgers <43591426+llainSregdor@users.noreply.github.com>
2022-06-10 16:12:50 +0930

    Delete public directory

--- public/javascripts/manage.js
+++
@@ -1,9 +0,0 @@
-function showPassword(){
-    var tickbox = document.getElementById("tickbox");
-    var password = document.getElementById("currentPassword");
-    if(tickbox.checked == true){
-        password.style.display = "block";
-    }else{
-        password.style.display = "none";
-    }
-}
